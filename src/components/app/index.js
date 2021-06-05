e3ac354cfc94bfd4246c53210280288a64584033
leonardovee <leoveeph@gmail.com>
2021-06-05 17:36:02 -0300

    feat: search page

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import AddBookPage from '../../pages/add-book-page'
 import HomePage from '../../pages/home-page'
+import SearchPage from '../../pages/search-page'
 import './index.css'
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
       <Switch>
         <Route exact path='/' component={ HomePage }/>
         <Route exact path='/add-book' component={ AddBookPage }/>
+        <Route path='/search' component={ SearchPage }/>
       </Switch>
     </BrowserRouter>
   </main>
