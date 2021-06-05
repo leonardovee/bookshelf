833db5583bd503ed4de04b647411b1d4caff1d28
leonardovee <leoveeph@gmail.com>
2021-06-05 15:38:50 -0300

    refactor: improve redability

--- src/components/input-text-area/index.js
+++ src/components/input-text-area/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import './index.css'
 
-const InputTextArea = (props) => (
-  <section className='input-text-area'>
-    <textarea className='input-text-area-box' placeholder={props.placeholder} rows={props.rows}/>
-  </section>
-)
+const InputTextArea = (props) => {
+  const [value, setValue] = useState('')
+  const input = (
+    <section className='input-text-area'>
+      <textarea value={value} onChange={e => setValue(e.target.value)} className='input-text-area-box' placeholder={props.placeholder} rows={props.rows}/>
+    </section>
+  )
+  return [value, input]
+}
 
 InputTextArea.propTypes = {
   placeholder: PropTypes.string,
--- src/components/input-text/index.js
+++ src/components/input-text/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import './index.css'
 
-const InputText = (props) => (
-  <section className='input-text'>
-    <input className='input-text-box' placeholder={props.placeholder}/>
-  </section>
-)
+const InputText = (props) => {
+  const [value, setValue] = useState('')
+  const input = (
+    <section className='input-text'>
+      <input value={value} onChange={e => setValue(e.target.value)} className='input-text-box' placeholder={props.placeholder}/>
+    </section>
+  )
+  return [value, input]
+}
 
 InputText.propTypes = {
   placeholder: PropTypes.string
