793dca0724448aa68785c92de6a37d8000176cbb
leonardovee <leoveeph@gmail.com>
2021-06-06 16:12:41 -0300

    feat: load more books

--- src/components/book-list/index.js
+++ src/components/book-list/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Grid } from '@material-ui/core'
 import BookListBook from '../book-list-book'
+import InputButtonSmall from '../input-button-small'
 import api from '../../services/api.js'
 import './index.css'
 
@@ -12,13 +13,21 @@ const BookList = () => {
   }, [setBooks])
 
   const getQuery = () => {
-    return window.location.search
+    const queryString = window.location.search
+    if (!queryString) return '?true'
+    return queryString
   }
 
   const load = async () => {
     const response = await api.get(`/books${getQuery()}`)
     if (!response.data) return
     setBooks(response.data)
+  }
+
+  const more = async () => {
+    const response = await api.get(`/books${getQuery()}&offset=${books.length}`)
+    if (!response.data[0]) return
+    setBooks(books => [...books, response.data[0]])
   }
 
   return (
@@ -30,6 +39,7 @@ const BookList = () => {
           </Grid>
         ))}
       </Grid>
+      <InputButtonSmall onClick={more} placeholder='Load more' />
     </div>
   )
 }
